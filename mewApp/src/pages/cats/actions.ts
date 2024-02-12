472e0be5f18b4297fb5e369f8af98a8d178e283f
Jakub Alkhaled <jacobkaled@yahoo.com>
2024-02-12 12:05:49 +0100

    inserting access key for more images

--- mewApp/src/pages/cats/actions.ts
+++ mewApp/src/pages/cats/actions.ts
@@ -1,16 +1,35 @@
 import { useQuery } from "@tanstack/react-query";
 
+const API_KEY =
+  "live_puw6ufBzDBnh6Wyde05s6J81oK1NS9lWzyignHoXN5yUhwYJeKPjLbCYF2zNMgIq";
+
 const fetchCats = async () => {
   const headers = new Headers({
     "Content-Type": "application/json",
-    "x-api-key": "DEMO-API-KEY",
+    "x-api-key": API_KEY,
   });
+
+  type Breed = {
+    weight: { imperial: string; metric: string };
+    id: string;
+    name: string;
+    temperament: string;
+    origin: string;
+    country_codes: string;
+    country_code: string;
+    life_span: string;
+    wikipedia_url: string;
+  };
 
   type CatsResp = Array<{
     height: number;
     id: string;
     url: string;
     width: number;
+    favourite?: any;
+    breeds: Array<Breed>;
+
+    // categories: Array<{ id: string; name: string }>;
   }>;
 
   const requestOptions: RequestInit = {
@@ -20,11 +39,11 @@ const fetchCats = async () => {
   };
 
   const res = await fetch(
-    "https://api.thecatapi.com/v1/images/search?limit=10",
+    "https://api.thecatapi.com/v1/images/search?limit=5",
     requestOptions
   );
   const res1 = await res.json();
-
+  console.log("data=>", res1);
   return res1 as CatsResp;
 };
 
