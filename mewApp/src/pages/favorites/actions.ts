ac5f817a9533ebbd2741d10bb5dbf1a60d54df92
Jakub Alkhaled <jacobkaled@yahoo.com>
2024-02-13 10:38:52 +0100

    displaying the favorite cats and implement the remove modal

--- mewApp/src/pages/favorites/actions.ts
+++ mewApp/src/pages/favorites/actions.ts
@@ -1,5 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import { API_KEY } from "../cats/actions";
+import { Favorite } from "@mui/icons-material";
+
+type Favorite = {
+  created_at: string;
+  id: number;
+  image: { id: string; url: string };
+  image_id: string;
+  sub_id: string | null;
+  user_id: string;
+};
 
 export const fetchFavs = async () => {
   const headers = new Headers({
@@ -12,11 +22,12 @@ export const fetchFavs = async () => {
     redirect: "follow",
   };
 
-  return fetch(`https://api.thecatapi.com/v1/favourites`, requestOptions)
-    .then((res) => res.json())
-    .then((data) => console.log("favs", data));
+  return fetch(`https://api.thecatapi.com/v1/favourites`, requestOptions).then(
+    (res) => res.json()
+  );
+  // .then((data) => console.log("favs", data));
 };
 
 export const useGetFavs = () => {
-  return useQuery<any>(["fetchIndividualCat"], fetchFavs);
+  return useQuery<Array<Favorite>>(["fetchIndividualCat"], fetchFavs);
 };
--- mewApp/src/pages/favorites/index.tsx
+++ mewApp/src/pages/favorites/index.tsx
@@ -1,8 +1,70 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogTitle,
+  Grid,
+} from "@mui/material";
 import { useGetFavs } from "./actions";
+import { useState } from "react";
 
 const Favorites = () => {
-  const { data } = useGetFavs();
-  return <div>this is cats FAAAVS</div>;
+  const { data, isLoading } = useGetFavs();
+  const [selectedFavCat, setSelectedFavCat] = useState<number | null>(null);
+  return (
+    <Grid>
+      {isLoading && <Grid> ... isloading</Grid>}
+      {data && (
+        <Grid>
+          {data.map((fav) => (
+            <Grid>
+              <img src={fav.image.url} />
+              <Button onClick={() => setSelectedFavCat(fav.id)}>
+                remove from favourite
+              </Button>
+            </Grid>
+          ))}
+        </Grid>
+      )}
+
+      <DeleteModal
+        title="do you want to remove this cat from favorite list?"
+        isLoading={false}
+        isDeleteModalOpen={!!selectedFavCat}
+        onCloseModal={() => setSelectedFavCat(null)}
+        onDeleteAttribute={() => null}
+      />
+    </Grid>
+  );
 };
+
+type DeleteModalProps = {
+  title: string;
+  isLoading: boolean;
+  isDeleteModalOpen: boolean;
+  onCloseModal: () => void;
+  onDeleteAttribute: () => void;
+};
+
+export function DeleteModal(props: DeleteModalProps) {
+  const {
+    isLoading,
+    title,
+    isDeleteModalOpen,
+    onCloseModal,
+    onDeleteAttribute,
+  } = props;
+  return (
+    <Dialog open={isDeleteModalOpen} maxWidth="sm" onClose={onCloseModal}>
+      <DialogTitle>{title}</DialogTitle>
+      <DialogActions>
+        <Button onClick={onCloseModal}>close</Button>
+        <Button onClick={onDeleteAttribute} autoFocus disabled={isLoading}>
+          {isLoading ? "Loading" : "delete"}
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
 
 export default Favorites;
